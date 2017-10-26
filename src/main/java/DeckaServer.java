d4a8687759b448e5aacd6cc1a07d8146c6dc39b8
nooni <normonds@gmail.com>
2017-10-26 22:39:48 +0300

    commit

--- src/main/java/DeckaServer.java
+++ src/main/java/DeckaServer.java
@@ -13,7 +13,8 @@ public static void main (String[] args) throws Exception {
 	Decka decka = new Decka();
 	decka.init();
 	//Decka.init();
-	Server server = new Server(System.getenv("PORT"));
+	int port = System.getenv("PORT");
+	Server server = new Server(port);
 	WebSocketHandler wsHandler = new WebSocketHandler () {
 		@Override public void configure (WebSocketServletFactory factory) {
 			factory.register(WebSocketImplementation.class);
